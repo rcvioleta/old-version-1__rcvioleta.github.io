d5a32312cac59eb5fae699ff0372db8cbdd94b62
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-04-07 23:54:37 +0800

    Major design modification

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger')
-
-const toggleActive = () => {
-  hamburger.classList.toggle('active')
-}
-
-hamburger.addEventListener('click', toggleActive, false)
